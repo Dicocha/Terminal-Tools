8b48e6969b7c146ee85fa974a0c0a5f65abbef7f
Dicocha <diegocoto27@outlook.com>
2022-05-19 13:57:35 -0600

    Fixed the error of update

--- TerminalTools.py
+++ TerminalTools.py
@@ -1,50 +1,15 @@
 from deep_translator import GoogleTranslator
-from cryptography.fernet import Fernet
 from functools import reduce
 import pyfiglet
 import os
 
 class Update:
-    key = Fernet.generate_key()
-
-    def Encrypt():
-        # Opened the file Thekey.key to read the key
-        with open("Thekey.key", "rb") as filekey: 
-            key = filekey.read() 
-        fernet = Fernet(key) 
-            
-        # Read the file to be encrypted
-        with open("Password.txt", "rb") as file: 
-            original = file.read()
-            
-        # Encrypted the file
-        encrypted = fernet.encrypt(original) 
-            
-        # Write the encrypted file
-        with open("Password.txt", "wb") as encrypted_file: 
-            encrypted_file.write(encrypted)
-
-    def Decrypt():
-        # Opened the file Thekey.key to read the key
-        with open("Thekey.key", "rb") as filekey: 
-            key = filekey.read()
-
-        # Reading the encrypted file
-        with open("Password.txt", "rb") as enc_file: 
-            Decrypt = enc_file.read()
-
-        # Decrypt the file and retorned the text
-        fernet = Fernet(key)
-        decrypted = fernet.decrypt(Decrypt)
-        return decrypted.decode()
-        
     def Main():
         # Declaring the variables
-        passw = Update.Decrypt()
         Yes = "y"
 
-        # This is the error 
-        os.system("echo %s |sudo -S pacman -Syyyyu" % passw)
+        # Update the system
+        os.system("echo {} |sudo pacman -Syyu".format(Yes))
 
         # These are the selections to choose from
         print("\nDo you want eraser cache?\n")
@@ -64,23 +29,6 @@ class Update:
 
         else:
             print("\nWrong choice")
-
-    def Make_dir():
-        os.mkdir("Resources")
-        os.chdir("Resources")
-
-        # Generate a key Fernet for encrypting password root
-        global key
-        key = Fernet.generate_key()
-        with open("Thekey.key","wb") as File_key:
-            File_key.write(key)
-
-        password = input("Type the root pasword: ")
-
-        with open("Password.txt","w") as File_key:
-            File_key.write(password)
-
-        Update.Encrypt()
 
 class Translate:
     def Main():
@@ -148,13 +96,6 @@ while True:
 
     if(switch == "1"):
         os.system("clear")
-
-        try:
-            os.chdir("Resources")
-
-        except:
-            Update.Make_dir()
-        
         Update.Main()
         os.chdir("..")
         os.system("clear")
